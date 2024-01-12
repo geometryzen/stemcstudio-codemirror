c8c7aad37ea47a613af9c077e0976214bf7c3f8d
David Geo Holmes <geometryzen@gmail.com>
2024-01-12 11:15:56 -0500

    Initial Commit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 
-export { javascript } from "@codemirror/lang-javascript";
 export { basicSetup, minimalSetup } from "codemirror";
 import { EditorView as CmEditorView } from "codemirror";
 
@@ -7,8 +6,56 @@ export interface Extension {
 
 }
 
+export interface Line {
+    from: number;
+    to: number;
+    number: number;
+    text: string;
+    length: number;
+}
+
 export interface Text {
+    length: number;
+    lines: number;
+    lineAt(n: number): Line;
+    replace(from: number, to: number, text: Text): Text;
+    append(other: Text): Text;
+    slice(from: number, to?: number): Text;
+    sliceString(from: number, to?: number, lineSep?: string): string;
+    eq(other: Text): boolean;
     toString(): string;
+    toJSON(): string[];
+}
+
+export interface EditorSelection {
+    ranges: SelectionRange;
+    mainIndex: number;
+    main: SelectionRange;
+    asSingle(): EditorSelection;
+    addRange(range: SelectionRange, main?: boolean): EditorSelection;
+    replaceRange(range: SelectionRange, which?: number): EditorSelection;
+}
+
+export interface ChangeDesc {
+    length: number;
+    newLength: number;
+    empty: boolean;
+    invertedDesc: ChangeDesc;
+    composeDesc(other: ChangeDesc): ChangeDesc;
+}
+
+export interface SelectionRange {
+    from: number;
+    to: number;
+    anchor: number;
+    head: number;
+    empty: boolean;
+    assoc: -1 | 0 | 1;
+    bidiLevel: number | null;
+    goalColumn: number | undefined;
+    map(change: ChangeDesc, assoc?: number): SelectionRange;
+    extend(from: number, to?: number): SelectionRange;
+    eq(other: SelectionRange, includeAssoc?: boolean): boolean;
 }
 
 export interface EditorState {
@@ -16,10 +63,16 @@ export interface EditorState {
      * The current document.
      */
     doc: Text;
+    selection: EditorSelection;
+    tabSize: number;
+    lineBreak: string;
+    readOnly: boolean;
+    wordAt(pos: number): SelectionRange | null;
 }
 
 export interface EditorStateConfig {
     doc?: string | Text;
+    selection?: EditorSelection | { anchor: number, head?: number };
     extensions?: Extension[];
 }
 
@@ -33,12 +86,31 @@ export interface ChangeSet {
 }
 
 export interface Transaction {
+    startState: EditorState;
     changes: ChangeSet | ChangeSet[];
+    selection: EditorSelection | undefined;
+    scrollIntoView: boolean;
+    newDoc: Text;
+    newSelection: EditorSelection;
+    state: EditorState;
+    docChanged: boolean;
+    reconfigured: boolean;
+    isUserEvent(event: string): boolean;
+}
+
+export type ChangeSpec = {
+    from: number;
+    to?: number;
+    insert?: string | Text;
+} | ChangeSet | readonly ChangeSpec[];
+
+export interface TransactionSpec {
+    changes?: ChangeSpec;
 }
 
 export interface EditorView {
     state: EditorState;
-    dispatch(tr: Transaction): void;
+    dispatch(...specs: TransactionSpec[]): void;
 }
 
 export function createEditorView(config?: EditorViewConfig): EditorView {
@@ -53,4 +125,8 @@ export interface StyleSpec {
 export function theme(spec: { [selector: string]: StyleSpec }, options?: { dark?: boolean }): Extension {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     return CmEditorView.theme(spec as any, options);
+}
+
+export interface TagStyle {
+
 }
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,10 +1,50 @@
-import { basicSetup, ChangeSet, createEditorView, EditorState, EditorView, EditorViewConfig, javascript, minimalSetup, Text, Transaction } from '../src/index';
+import { basicSetup, ChangeSet, createEditorView, EditorSelection, EditorState, EditorView, EditorViewConfig, Line, minimalSetup, SelectionRange, Text, Transaction } from '../src/index';
 
 class TestText implements Text {
+    length: number;
+    lines: number;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    lineAt(n: number): Line {
+        throw new Error('Method not implemented.');
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    replace(from: number, to: number, text: Text): Text {
+        throw new Error('Method not implemented.');
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    append(other: Text): Text {
+        throw new Error('Method not implemented.');
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    slice(from: number, to?: number): Text {
+        throw new Error('Method not implemented.');
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    sliceString(from: number, to?: number, lineSep?: string): string {
+        throw new Error('Method not implemented.');
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    eq(other: Text): boolean {
+        throw new Error('Method not implemented.');
+    }
+    toString(): string {
+        return "Lorem ...";
+    }
+    toJSON(): string[] {
+        throw new Error('Method not implemented.');
+    }
 
 }
 
 class TestEditorState implements EditorState {
+    selection: EditorSelection;
+    tabSize: number;
+    lineBreak: string;
+    readOnly: boolean;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    wordAt(pos: number): SelectionRange {
+        throw new Error('Method not implemented.');
+    }
     readonly doc: Text = new TestText();
 }
 
@@ -28,10 +68,6 @@ test("create_editor_view is a function", function () {
 
 test("basicSetup is an object", function () {
     expect(typeof basicSetup).toBe('object');
-});
-
-test("javascript is an object", function () {
-    expect(typeof javascript).toBe('function');
 });
 
 test("minimalSetup is an object", function () {
